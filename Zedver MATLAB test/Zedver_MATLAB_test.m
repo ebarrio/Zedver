253d3651edb3f745db1daed7f26b2da7e0c6988e
Eladio Barrio Querol <47576771+ebarrio@users.noreply.github.com>
2019-03-04 16:59:50 +0100

    Update Zedver_MATLAB_test.m

--- Zedver MATLAB test/Zedver_MATLAB_test.m
+++ Zedver MATLAB test/Zedver_MATLAB_test.m
@@ -1,4 +1,4 @@
-%     Copyright (C)  2019  Eladio Barrio Querol.
+%    Copyright (C)  2019  Eladio Barrio Querol.
 %    Permission is granted to copy, distribute and/or modify this document
 %    under the terms of the GNU Free Documentation License, Version 1.3
 %    or any later version published by the Free Software Foundation;
