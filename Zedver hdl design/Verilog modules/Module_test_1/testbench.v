a01151aedecd6bb2c5922c4ea671448c9d85d299
Eladio Barrio Querol <47576771+ebarrio@users.noreply.github.com>
2019-03-07 20:35:10 +0100

    Update testbench.v

--- Zedver hdl design/Verilog modules/Module_test_1/testbench.v
+++ Zedver hdl design/Verilog modules/Module_test_1/testbench.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.03.2019 20:18:26
-// Design Name: 
-// Module Name: testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module testbench();
 reg A;
 wire [2:0] sel;
