4d9f154befb2e119b4bf734d98a22b534a74d6aa
Eladio Barrio Querol <47576771+ebarrio@users.noreply.github.com>
2019-03-07 20:35:29 +0100

    Update top.v

--- Zedver hdl design/Verilog modules/Module_test_1/top.v
+++ Zedver hdl design/Verilog modules/Module_test_1/top.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.03.2019 20:15:07
-// Design Name: 
-// Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module top(
     input [23:0] A,
     output [2:0] sel
