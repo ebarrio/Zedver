90af269a8356caddaf8d96ee5e9bd4a3a0420aaf
Eladio Barrio Querol <47576771+ebarrio@users.noreply.github.com>
2019-04-01 15:46:51 +0200

    Update tb_histogram_1_v1.v

--- Zedver hdl design/Verilog modules/Histogram_v1/tb_histogram_1_v1.v
+++ Zedver hdl design/Verilog modules/Histogram_v1/tb_histogram_1_v1.v
@@ -3,9 +3,9 @@
 module testbench();
 
 reg clk = 0;
-reg rst = 1;        //-- Señal de reloj global
+reg rst = 1;        //-- SeÃ±al de reloj global
 reg [7: 0] addr = 8'b0;    //-- Direccion = Bin
-reg rw = 0;             //-- Modo lectura (1) o escritura/adquisición (0)
+reg rw = 0;             //-- Modo lectura (1) o escritura/adquisiciÃ³n (0)
 reg [15: 0] data_in = 16'b0;   //-- Dato lectura
 wire [15: 0] data_out; //-- Dato escritura
 
@@ -20,14 +20,26 @@ histogram_v1 dut(
 integer k = 0;
 integer NPOS = 2 ** 8; // Numero bins
 initial begin
-#20 //Leemos estado inicial memoria
+//#20 //Leemos estado inicial memoria
+//rw = 1;
+//for (k = 0; k < NPOS ; k = k + 1) begin
+//#4 clk = ~ clk;
+//addr = NPOS;
+//end
+#20 //Escribimos adquisicion todo ceros, data in a 0 al inicio.
+rw = 0;
+for (k = 0; k < NPOS ; k = k + 1) begin
+#4 clk = ~ clk;
+addr = NPOS;
+end
+#20 //Leemos estado final memoria
 rw = 1;
 for (k = 0; k < NPOS ; k = k + 1) begin
 #4 clk = ~ clk;
 addr = NPOS;
 end
 #20 rst = ~ rst;
-rst = ~ rst; // Reiniciamos y volvemos a leer
+#4 rst = ~ rst; // Reiniciamos y volvemos a leer
 rw = 1;
 for (k = 0; k < NPOS ; k = k + 1) begin
 #4 clk = ~ clk;
